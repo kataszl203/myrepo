20d39f1ded35e58bb1e38cac8a4402b8bb2e7423
Kasia Szleper <szleperkasia@gmail.com>
2021-05-30 12:34:16 +0200

    Tabsets

--- server.R
+++ server.R
@@ -1,2 +1,34 @@
+library(shiny)
+library(datasets)
+
+
 shinyServer(function(input, output) {
+
+  formulaText <- reactive({
+    paste("mpg ~", input$variable)
+  })
+
+  # Return the formula text for printing as a caption
+  output$caption <- renderText({
+    formulaText()
+  })
+
+  # Generate a plot of the requested variable against mpg and only 
+  # include outliers if requested
+  output$mpgPlot <- renderPlot({
+    boxplot(as.formula(formulaText()), 
+            data = mpgData,
+            outline = input$outliers)
+  })
+  sliderValues <- reactive({
+      #Compose data frame
+      data.frame(
+          Name =c("Integer"),
+          Value = as.character(c(input$integer)),
+          stringsAsFactors = FALSE)
+  })
+  # Show the values using an HTML table
+  output$values <- renderTable({
+    sliderValues()
+  })
 })
--- ui.R
+++ ui.R
@@ -1,14 +1,30 @@
 library(shiny)
 
-shinyUI(fluidPage(
-  titlePanel("Moja pierwsza aplikacja"),
-  sidebarLayout(
-    sidebarPanel(
-      p("Tutaj jest lewy panel")
+# Define UI for microarrays data analysis application
+shinyUI(pageWithSidebar(
+
+  # Application title
+  headerPanel("Analiza danych mikromacierzowych z wykorzystaniem
+   klasy ExpressionSet"),
+  # Sidebar with controls to select the variable to plot against mpg
+  # and to specify whether outliers should be included
+  sidebarPanel(
+        sliderInput("integer", "Integer:",
+                min=0, max=100, value=20),
+    br(),
+    checkboxInput("outliers", "Pokaż klasę poszczególnych danych",
+     FALSE)
+  ),
+
+  # Show a tabset that includes a plot, summary, and table view
+  # of the generated distribution
+  mainPanel(
+    tabsetPanel(
+      tabPanel("Wykres", plotOutput("plot")),
+      tabPanel("Podsumowanie", verbatimTextOutput("summary")),
+      tabPanel("Tabela", tableOutput("table"))
     ),
-    mainPanel(
-      br(),
-      p("Tutaj jest prawy panel")
-    )
+    h3(textOutput("caption")),
+
   )
 ))
