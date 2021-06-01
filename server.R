f81aa18922165bfd0b1a510803b302ef1ff70a1c
Kasia Szleper <szleperkasia@gmail.com>
2021-06-01 20:09:14 +0200

    Download button and tabsets

--- server.R
+++ server.R
@@ -4,31 +4,35 @@ library(datasets)
 
 shinyServer(function(input, output) {
 
-  formulaText <- reactive({
-    paste("mpg ~", input$variable)
-  })
-
-  # Return the formula text for printing as a caption
-  output$caption <- renderText({
-    formulaText()
-  })
-
-  # Generate a plot of the requested variable against mpg and only 
-  # include outliers if requested
-  output$mpgPlot <- renderPlot({
-    boxplot(as.formula(formulaText()), 
-            data = mpgData,
-            outline = input$outliers)
-  })
+  #Reactive value of slider (Skala)
   sliderValues <- reactive({
-      #Compose data frame
-      data.frame(
+    data.frame(
           Name =c("Integer"),
           Value = as.character(c(input$integer)),
           stringsAsFactors = FALSE)
   })
-  # Show the values using an HTML table
-  output$values <- renderTable({
+  #Reactive value of radio buttons (Rodzaj normalizacji)
+  d <- reactive({
+    switch(input$dist,
+           mas5 = rnorm,
+           rma = runif,
+           rnorm)
+    dist(input$integer)
+  })
+
+  output$plot <- renderPlot({
+    dist <- input$dist
+    integer <- input$integer
+
+    hist(d(),
+      main = paste("r", dist, "(",integer,")", sep = ""), 
+      col = "#75AADB", border = "white")
+  })  
+  output$summary <- renderPrint({
+    summary()
+  })
+
+  output$table <- renderTable({
     sliderValues()
   })
 })
--- ui.R
+++ ui.R
@@ -1,30 +1,29 @@
 library(shiny)
 
-# Define UI for microarrays data analysis application
-shinyUI(pageWithSidebar(
+shinyUI(fluidPage(
 
-  # Application title
-  headerPanel("Analiza danych mikromacierzowych z wykorzystaniem
-   klasy ExpressionSet"),
-  # Sidebar with controls to select the variable to plot against mpg
-  # and to specify whether outliers should be included
-  sidebarPanel(
-        sliderInput("integer", "Integer:",
-                min=0, max=100, value=20),
-    br(),
-    checkboxInput("outliers", "Pokaż klasę poszczególnych danych",
-     FALSE)
+  titlePanel("Analiza danych mikromacierzowych"),
+
+  sidebarLayout(
+    sidebarPanel(
+      radioButtons("dist", "Rodzaj normalizacji",
+                   c("MAS-5" = "mas5",
+                     "RMA" = "rma")),
+      br(),
+      sliderInput("integer", "Skala:",
+                  min = 0, max = 100, value = 20),
+      br(),
+      checkboxInput("outliers", "Pokaż klasę poszczególnych danych", FALSE),
+      br(),
+      downloadButton("button", "Pobierz raport")
   ),
 
-  # Show a tabset that includes a plot, summary, and table view
-  # of the generated distribution
-  mainPanel(
-    tabsetPanel(
-      tabPanel("Wykres", plotOutput("plot")),
-      tabPanel("Podsumowanie", verbatimTextOutput("summary")),
-      tabPanel("Tabela", tableOutput("table"))
-    ),
-    h3(textOutput("caption")),
-
+    mainPanel(
+      tabsetPanel(
+        tabPanel("Wykres", plotOutput("plot")),
+        tabPanel("Podsumowanie", verbatimTextOutput("summary")),
+        tabPanel("Tabela", tableOutput("table"))
+        ),
+    )
   )
 ))
